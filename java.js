07c55c6836c64ce0008f40e7ccee8440b75df613
JOCECODE <ulah57@gmail.com>
2020-07-18 21:06:01 -0700

    Confirms

--- java.js
+++ java.js
@@ -1,3 +1,18 @@
+var length = prompt("How long do you want your password? (Number value from 8-128).");
+var special = confirm("Do you want any special characters");
+var num = confirm("Do you want any numbers?");
+var uppercase = confirm("Do you want uppercase");
+var lowercase = confirm("Do you want any lowercase");
+
+// var txt;
+// var r = confirm("Press a button!");
+// if (r == true) {
+//   txt = "You pressed OK!";
+// } else {
+//   txt = "You pressed Cancel!";
+// }
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
