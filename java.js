ab5ce5f0aa40a621bc0eda9d87628fcda1dcfa78
JOCECODE <ulah57@gmail.com>
2020-07-18 20:39:09 -0700

    Skeleton build

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-var password = prompt("Welcome to Jose U Perez Jr. Password generator. How many characters would you like in your pasword pick 8-128")
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
